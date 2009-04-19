1cb6c3197b7567c150ce2aaba90fb57aedb3b1e6
James Webber <bunkerprivate@googlemail.com>
2009-04-19 20:02:05 +0100

    Mess about with colours.

--- print-git-modified.sh
+++ print-git-modified.sh
@@ -5,6 +5,16 @@ CODE_DIR="/home/bunker/src"
 
 COL_NONE="\\033[0;0m"
 COL_RED="\\033[0;31m"
+COL_BLUE="\\033[0;34m"
+COL_GREEN="\\033[0;32m"
+
+COL_RED_UL="\\033[4;31m"
+
+COL_LBLUE="\\033[1;34m"
+COL_LYELLOW="\\033[1;33m"
+COL_LGREEN="\\033[1;32m"
+COL_LRED="\\033[1;31m"
+COL_LCYAN="\\033[1;36m"
 
 for dir in `echo ${CODE_DIR}/*`; do
   gitdir=${dir}/.git
@@ -12,10 +22,10 @@ for dir in `echo ${CODE_DIR}/*`; do
     cd $dir
     files=`git status --untracked-files=no | grep '#\s*\(modified\|new\|deleted\):' | sed 's/#\s*\(.*\)/\1/'`
     if test -n "${files}"; then
-      echo "${COL_RED} * $dir${COL_NONE}"
+      echo "${COL_LCYAN} * $dir${COL_NONE}"
       descr=`git describe 2> /dev/null`
       if test $? -eq 0; then
-        echo "- ${descr}"
+        echo "${COL_GREEN}- ${descr}${COL_NONE}"
       fi
       echo "${files}"
     fi
