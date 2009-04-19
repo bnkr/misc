ccdfdc024fdf16676a9be3e133e209c67e445b64
James Webber <bunkerprivate@googlemail.com>
2009-04-19 19:41:20 +0100

    Print git-describe if it exists.

--- print-git-modified.sh
+++ print-git-modified.sh
@@ -3,13 +3,20 @@
 
 CODE_DIR="/home/bunker/src"
 
+COL_NONE="\\033[0;0m"
+COL_RED="\\033[0;31m"
+
 for dir in `echo ${CODE_DIR}/*`; do
   gitdir=${dir}/.git
   if test -d $gitdir -a \( ! -L $dir \); then
     cd $dir
     files=`git status --untracked-files=no | grep '#\s*\(modified\|new\|deleted\):' | sed 's/#\s*\(.*\)/\1/'`
     if test -n "${files}"; then
-      echo "\\033[0;31m * $dir\\033[0;0m"
+      echo "${COL_RED} * $dir${COL_NONE}"
+      descr=`git describe 2> /dev/null`
+      if test $? -eq 0; then
+        echo "- ${descr}"
+      fi
       echo "${files}"
     fi
     cd ..
